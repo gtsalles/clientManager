26aa6b79d651c4f35445c32864093fa14d46e7da
Gustavo Carvalho <gt.salles@gmail.com>
2013-03-08 01:02:41 -0300

    Models created

--- client/models.py
+++ client/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    sex = models.CharField('Sexo', max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    birthday = models.DateField()
+    email = models.EmailField()
+    cpf = models.CharField('CPF', max_length=11)
+    address = models.ForeignKey('Address')
+    phone = models.ForeignKey('Phone')
+
+class Address(models.Model):
+    street = models.CharField('Rua', max_length=50)
+    number = models.IntegerField()
+    neighborhood = models.CharField('Bairro', max_length=50)
+    zone = models.CharField('Zona', max_length=50)
+    city = models.ForeignKey('City')
+
+class City(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    state = models.ForeignKey('State')
+
+class State(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    acr = models.CharField('Sigla', max_length=2)
+
+class Phone(models.Model):
+    number = models.CharField('numero', max_length=11)
