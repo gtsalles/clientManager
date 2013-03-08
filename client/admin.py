b79ed3afcf4ec9e08c4f318b0eb3d9a4e2fbb461
Gustavo Carvalho <gt.salles@gmail.com>
2013-03-08 11:41:40 -0300

    More changes in model

---
+++ client/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from client.models import *
+
+
+
+admin.site.register(Client)
+admin.site.register(Address)
+admin.site.register(City)
+admin.site.register(State)
+admin.site.register(Phone)
--- client/models.py
+++ client/models.py
@@ -7,22 +7,52 @@ class Client(models.Model):
     email = models.EmailField()
     cpf = models.CharField('CPF', max_length=11)
     address = models.ForeignKey('Address')
-    phone = models.ForeignKey('Phone')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
 class Address(models.Model):
     street = models.CharField('Rua', max_length=50)
-    number = models.IntegerField()
+    number = models.IntegerField('Numero')
     district = models.CharField('Bairro', max_length=50)
     zone = models.CharField('Zona', max_length=50)
     city = models.ForeignKey('City')
+
+    def __unicode__(self):
+        return u'%s %s' % (self.number, self.street)
+
+    class Meta:
+        ordering = ['street', 'number']
 
 class City(models.Model):
     name = models.CharField('Nome', max_length=50)
     state = models.ForeignKey('State')
 
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
 class State(models.Model):
     name = models.CharField('Nome', max_length=50)
     acr = models.CharField('Sigla', max_length=2)
 
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
 class Phone(models.Model):
     number = models.CharField('Numero', max_length=11)
+    client = models.ForeignKey('Client')
+
+    def __unicode__(self):
+        return self.number
+
+    class Meta:
+        ordering = ['number']
