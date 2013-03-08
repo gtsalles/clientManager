3a2bf434c1c84359388fbb814319b5668885d2a0
Gustavo Carvalho <gt.salles@gmail.com>
2013-03-08 11:08:47 -0300

    Change in some attributes of a few models

--- client/models.py
+++ client/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField('Nome', max_length=100)
-    sex = models.CharField('Sexo', max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    sex = models.CharField('Sexo', max_length=1, choices=(('M', 'Masculino'), ('F', 'Feminino')))
     birthday = models.DateField()
     email = models.EmailField()
     cpf = models.CharField('CPF', max_length=11)
@@ -12,7 +12,7 @@ class Client(models.Model):
 class Address(models.Model):
     street = models.CharField('Rua', max_length=50)
     number = models.IntegerField()
-    neighborhood = models.CharField('Bairro', max_length=50)
+    district = models.CharField('Bairro', max_length=50)
     zone = models.CharField('Zona', max_length=50)
     city = models.ForeignKey('City')
 
@@ -25,4 +25,4 @@ class State(models.Model):
     acr = models.CharField('Sigla', max_length=2)
 
 class Phone(models.Model):
-    number = models.CharField('numero', max_length=11)
+    number = models.CharField('Numero', max_length=11)
